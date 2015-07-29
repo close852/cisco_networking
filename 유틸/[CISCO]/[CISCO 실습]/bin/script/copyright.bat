14dcfdebd2edc390e37dcfc39a80c8507764ebba
close852 <close852@naver.com>
2015-07-29 09:35:32 +0900

    CISCO 파일 삭제/ 다 안올라가서 그냥 메일로 보냈음

--- 유틸/[CISCO]/[CISCO 실습]/bin/script/copyright.bat
+++
@@ -1,6 +0,0 @@
-%%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a 
-cls
-@echo off
-cls
-title    SOLDESK IT Academy Cisco Training (Cisco Network Dynamips LAB)
-echo *=============================================================================*
